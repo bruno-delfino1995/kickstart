7d5b72cfe24187af18cd729d8989f63df7f9f0f9
Bruno Delfino <bruno.delfino1995@gmail.com>
2025-07-31 10:46:59 -0300

    nvim: remove null-ls formatting

--- modules/editor/common/user/.config/nvim/lua/plugins/format.lua
+++ modules/editor/common/user/.config/nvim/lua/plugins/format.lua
@@ -1,25 +1,4 @@
 return {
   -- editorconfig for vim
   'editorconfig/editorconfig-vim',
-
-  -- formatting as if it were a language server
-  {
-    'jose-elias-alvarez/null-ls.nvim',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-    },
-    config = function()
-      local null_ls = require('null-ls')
-
-      local formatting = null_ls.builtins.formatting
-      local diagnostics = null_ls.builtins.diagnostics
-
-      null_ls.setup({
-        sources = {
-          formatting.stylua,
-          -- diagnostics.credo
-        },
-      })
-    end,
-  },
 }
